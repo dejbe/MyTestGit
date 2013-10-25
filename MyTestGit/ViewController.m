45b47031442838d3324084cb031ce68a7b2d310a
Jonas Desir <diamonddallas_10@yahoo.com>
2013-10-25 19:23:22 -0400

    I just add more code

--- MyTestGit/ViewController.m
+++ MyTestGit/ViewController.m
@@ -18,6 +18,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    //I'm adding more code'
 }   // I'm adding code now
 
 - (void)didReceiveMemoryWarning
