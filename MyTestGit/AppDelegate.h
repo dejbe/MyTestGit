afc86f15e8d1b2c06052b1e700335cbbc3853f5e
Jonas Desir <diamonddallas_10@yahoo.com>
2013-10-25 18:37:19 -0400

    Initial Commit

---
+++ MyTestGit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyTestGit
+//
+//  Created by Jonas D on 10/25/13.
+//  Copyright (c) 2013 DEJBE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyTestGit/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MyTestGit
+//
+//  Created by Jonas D on 10/25/13.
+//  Copyright (c) 2013 DEJBE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
